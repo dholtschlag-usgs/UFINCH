37f9dd71cd0bb28f27ad7d8544808e71a11595fd
dholtschlag-usgs <dholtschlag@usgs.gov>
2014-04-23 11:54:46 -0400

    Updated EqnGenerator.m

--- EqnGenerator.m
+++ EqnGenerator.m
@@ -1,7 +1,11 @@
 % The equation generator is code used to revise the algorithm for
 % developing equations used in UFinch simulations. 
 % Dave Holtschlag, Apr. 23, 2014
-
+%
+% Extract data from hUFinchGUI workspace following UFINCH run.
+hUFinchGUI    = getappdata(0,'hUFinchGUI');
+info          = getappdata(hUFinchGUI);
+LevelPathSet  = info.LevelPathSet; 
 
 % Compute the length (number of branches) of the LevelPathSet
 lenLevelPathSet = length(LevelPathSet);
@@ -20,8 +24,31 @@ for i = 1:lenLevelPathSet
         num2str(LevelPathSet(i).CumTT(j),'%03d'),') * ',...
         num2str(LevelPathSet(i).AreaSqKm(j),'%7.2f')] );
 end
-% 
+%
+j = 2;  % Index of minimum number of 
+% i is the index of lenLevelPathSet
+i = 1;
+% Find indices of branches that contain more than one (most upstream) flowline
 ndxLong = find(vecLevelPathSet >= 2);
+fprintf(1,'%s\n',['There are ',num2str(length(ndxLong)),' branches with ',num2str(j),' or more flowlines.']);
+%
+fprintf(1,'%s\n',['The index of the first branch is ',num2str(ndxLong(i)),'.']);
+tarFromNode = LevelPathSet(ndxLong(i)).FromNode{j};
+fprintf(1,'%s\n',['  The corresponding FromNode is ',tarFromNode,'.']);
+% 
+% 
+
+% Find the index of ToNodes that match the FromNode
+% Find the target node in all ToNode
+ToNodeSet = horzcat({LevelPathSet.ToNode});
+% Find the target node in all ToNode
+for k = 1:length(ndxLong)
+    fprintf(1,'%d %d %d \n',k,ndxLong(k),find(strcmp(tarFromNode,ToNodeSet{k}(1:end))==1));
+end
+
+
+
+
 % j indexes the "column" in the cell array Experiment with matching ToNode and FromNode
 j = 2;
 for i = 1:length(ndxLong)
@@ -31,10 +58,6 @@ for i = 1:length(ndxLong)
 tarFromNode = LevelPathSet(1).FromNode(j); 
 % 
 
-% Find the target node in all ToNode
-ToNodeSet = {LevelPathSet.ToNode};
-% Find the target node in all ToNode
-strcmp(tarFromNode,{horzcat(LevelPathSet.ToNode)})
 
 
 
