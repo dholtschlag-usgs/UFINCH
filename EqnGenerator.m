34c61102829ef89443e872cf7974a1e4bca3e4f6
dholtschlag-usgs <dholtschlag@usgs.gov>
2014-04-25 09:34:49 -0400

    EqnGenerator.m generates correct equations.

--- EqnGenerator.m
+++ EqnGenerator.m
@@ -115,4 +115,77 @@ for i = 2:max(vecLevelPathSet)
 end
 
 
+%% Algorithm for generating flow equations
+%
+% Initally, just loop through the network formed by LevelPathSet (LPS)
+%   Rows of LPS are branches, Cols are flowlines in downstream order
+%
+% Determine the number of branches in LPS
+numBrLPS = length(LevelPathSet);
+% Allocate numFlowLineBr to contain the number of flowlines in each branch
+numFlowLineBr   = nan(numBrLPS,1);
+% Float down the branches naming the ComIDs
+for i = 1:numBrLPS
+    % Determine the number of flowlines in the ith branch
+    numFlowLineBr(i) = length(LevelPathSet(i).FromNode);
+end
+% Compute the cumulative sum of flowlines in branches
+cumFlowLineBr        = cumsum(numFlowLineBr); 
+    % Print out the branch number and number of flowlines
+    % fprintf(1,'\n %u %u \n',i,numFlowLineBr(i));
+    % Starting with the second flowline, list the ComID
+    %     for j = 2:numFlowLineBr(i)
+    %         fprintf(1,'%d %s, ',j,LevelPathSet(i).FromNode{j});
+    %     end
+for i = 1:numBrLPS
+    % Print the branch number and headwater ComID
+%     fprintf(1,'%d %d \n',i,LevelPathSet(i).ComID(1));
+    % Starting with the second flowline in the branch, find all the
+    %  ComIDs in which their ToNode matches the current flowlines FromNode
+    % Compute yield equations for all flowlines 
+    % if numFlowLineBr(i) == 1
+    FlowBldEqn = ['C',num2str(LevelPathSet(i).ComID(1)),...
+            '(t-',num2str(LevelPathSet(i).CumTT(1),'%03d'),') = Ylds(t-',...
+            num2str(LevelPathSet(i).CumTT(1),'%03d'),') * ',...
+            num2str(LevelPathSet(i).AreaSqKm(1),'%8.3f')];
+            fprintf(1,'%s \n',[FlowBldEqn,';']);
+
+    % else
+    % Add info on any upstream flowlines
+    for j = 2:numFlowLineBr(i)
+        % Set current FromNode to targetToNode
+        targetToNode = LevelPathSet(i).FromNode(j);
+        targetComID  = LevelPathSet(i).ComID(j);
+        % Find the vector of indices of targetToNode in LSP
+        ndxToNode = find(ismember([LevelPathSet.ToNode],targetToNode)==1);
+        % Loop over vecToNode to find branch numBr and flowline numFL
+        FlowBldEqn = ['C',num2str(LevelPathSet(i).ComID(j)),...
+            '(t-',num2str(LevelPathSet(i).CumTT(j),'%03d'),') = Ylds(t-',...
+            num2str(LevelPathSet(i).CumTT(j),'%03d'),') * ',...
+            num2str(LevelPathSet(i).AreaSqKm(j),'%8.3f')];
+        for k = 1:length(ndxToNode)
+            % Branch with matching ToNode
+            numBr = find(ndxToNode(k) <= cumFlowLineBr,1,'first');
+            % Flowline in Branch with matching ToNode
+            if ndxToNode(k) > numFlowLineBr(1)
+                numFL = ndxToNode(k) - cumFlowLineBr(numBr-1);
+            else
+                numFL = ndxToNode(k);
+            end
+%             fprintf(1,'%3d %3d %3d %3d %3d %d %d \n',i,j,k,numBr,numFL,...
+%                 LevelPathSet(numBr).ComID(numFL),targetComID);
+            FlowBldEqn = horzcat(FlowBldEqn, ...
+                [' + C',num2str(LevelPathSet(numBr).ComID(numFL)),...
+                '(t-',num2str(LevelPathSet(numBr).CumTT(numFL),'%03d'),...
+                ')']);
+        end
+        fprintf(1,'%s \n',[FlowBldEqn,';']);
+    end
+        % print the FlowBldEqn (w/o) local water yield
+    % end
+end
+
+
+
+
 
